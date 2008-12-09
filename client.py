6dfd67914a7a1233e6e8b004362c1d61edc3caf4
henry.robinson <henry.robinson@7af600d8-c610-11dd-8bad-cde060d8fd3c>
2008-12-09 16:46:07 +0000

    Initial checkin, full of bugs and ugliness

---
+++ client.py
@@ -0,0 +1,79 @@
+import socket, pickle, memnode, random, threading, time, md5
+
+class ConcertoClient( object ):
+    retryCount = 5
+    def __init__( self ):
+        self.socket = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+        
+    def do_transaction( self, mmt ):
+        outstanding = {}
+        for lmt in mmt.lmts.values( ):
+            txt = pickle.dumps( ("EXEC", mmt.id, lmt.compares, lmt.reads, lmt.writes, lmt.lid ) )            
+            self.socket.sendto( txt, lmt.address )
+        nr = 0
+        abort = False
+        while nr < len(mmt.lmts):
+            (resp, addr) = self.socket.recvfrom( 2048 )        
+            resp = pickle.loads( resp )
+            print "Received response: ", resp
+            nr += 1
+            if resp[1][1] != memnode.VOTE_OK: 
+                if resp[1][1] == memnode.BAD_LOCK:
+                    lmt = mmt.lmts[ resp[0] ]
+                    if lmt.retry_count < retryCount:
+                        lmt.retry_count += 1
+                        nr -= 1
+                        txt = pickle.dumps( ("EXEC", mmt.id, lmt.compares, lmt.reads, lmt.writes, lmt.lid ) )            
+                        self.socket.sendto( txt, lmt.address )                        
+                    else:
+                        abort = True
+                        break
+                else:
+                    abort = True
+                    break
+        if abort:
+            command = "ABRT"
+        else:
+            command = "COMT"
+        for lmt in mmt.lmts.values( ):
+            txt = pickle.dumps( (command, mmt.id) )
+            self.socket.sendto( txt, lmt.address )
+        return (not abort, resp[1])
+    
+if __name__ == "__main__":
+    class test_thread( threading.Thread ):
+        tid = 0
+        ntrials = 10
+        host = "localhost"
+        def __init__( self ):
+            threading.Thread.__init__( self )
+            self.st = test_thread.tid * test_thread.ntrials
+            self.thid = test_thread.tid
+            test_thread.tid += 1
+            self.time = time.time( )
+            self.transident = (int((self.time * 1000000.0) % 1000000) << 16) + (self.thid << 8)
+        def run( self ):
+            print "Running thread %s" % self.thid
+            c = ConcertoClient( )
+            for n in xrange( self.st, self.st + test_thread.ntrials ):
+                tid = self.transident + n
+                m = memnode.MultiMiniTransaction( tid )
+                l = memnode.LocalMiniTransaction( {},{0:0,10:0,20:0}, {random.randint(0,999):random.randint(0,512)}, tid ,(test_thread.host, 21567))
+                l2 = memnode.LocalMiniTransaction( {},{0:0,10:0,20:0}, {random.randint(0,999):random.randint(0,512)}, tid ,(test_thread.host, 21568))
+                m.add_transaction( l )
+                m.add_transaction( l2 )
+                print "Starting transaction %s" % tid
+                success = c.do_transaction( m )
+                print "Transaction %s :: successful == %s (retry count :: %s)" % (tid,success,m.retry_count( ))
+            print "Thread %s finished!" % self.thid
+
+    num_threads = 5
+
+    threads = [ test_thread( ) for x in xrange(num_threads) ]
+    for t in threads:
+        t.start( )
+
+    for t in threads:
+        if t.isAlive( ):
+            t.join( )
+        
